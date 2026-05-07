5e8cd118368a53eb5c1a99bca73fb1bd9e25cbea
Cadenzathena <justinaddeh@gmail.com>
2026-05-07 09:35:34 +0100

    Fix main function regeneration

--- script.js
+++ script.js
@@ -4,26 +4,41 @@ const gridFrame = document.querySelector("#grid-frame");
 let gridSizeValue;
 let miniGrid;
 
+
+
+// button is the only/major thing that signals generation. This is a
+// simple function that sends a signal to main() that the btn has been clicked
 function generateBtnClicked() {
     return new Promise (resolve => {generateBtn.addEventListener("click",
         () => {resolve(gridSizeValue = +pickerField.value)}
     )})
 }
 
+
+
+// Mostly handles generation. gridFrameWidth determines how the miniGrids are arranged.
+// I made a sanitizer function to handle pulling the length of the miniGrid. All to
+// dynamically set the width of the gridFrame (maybe in case I changed the value of the miniGrid length in the future...)
 async function main() {
     await generateBtnClicked();
+
+    if (document.body.contains(miniGrid)) {
+        gridFrame.replaceChildren();
+    }
 
     for (let i = 0; i < gridSizeValue ** 2; i++) {
         miniGrid = document.createElement("div");
         gridFrame.appendChild(miniGrid);
     }
 
-    function gridFrameWidthSanitizer() {
+    function miniGridLengthSanitizer() {
         let miniGridLength = getComputedStyle(miniGrid).getPropertyValue("padding");
         return 2 * Number(miniGridLength.replace("px",""));
     }
 
-    gridFrame.style.width = `${gridSizeValue * gridFrameWidthSanitizer()}px`
+    gridFrame.style.width = `${gridSizeValue * miniGridLengthSanitizer()}px`
+
+    main();
 }
 
 main()
