0559d9af168a9a31f7940bc88d7357e16ab9cad1
Cadenzathena <justinaddeh@gmail.com>
2026-05-07 15:47:20 +0100

    Add hover effect for miniGrid
    Add input field enter key or button events to submit grid value

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+const pickerFrame = document.querySelector("#picker-frame");
 const pickerField = document.querySelector("#picker-field");
-const generateBtn = document.querySelector("div#picker-frame button");
+const generateBtn = document.querySelector("#picker-frame button");
 const gridFrame = document.querySelector("#grid-frame");
 let gridSizeValue;
 let miniGrid;
 
 
 
-// button is the only/major thing that signals generation. This is a
-// simple function that sends a signal to main() that the btn has been clicked
+// input field and it's button are the major things that signals generation. generateBtn
+// has the type="submit" attribute, allowing for an easy addEventListener "submit" to handle
+// clicking the button OR pressing enter. preventDefault() is just to prevent a page refresh.
 function generateBtnClicked() {
-    return new Promise (resolve => {generateBtn.addEventListener("click",
-        () => {resolve(gridSizeValue = +pickerField.value)}
+    return new Promise (resolve => {pickerFrame.addEventListener("submit",
+        (e) => {
+        e.preventDefault();
+        resolve(gridSizeValue = +pickerField.value);
+        }
     )})
 }
 
@@ -21,6 +26,8 @@ function generateBtnClicked() {
 // dynamically set the width of the gridFrame (maybe in case I changed the value of the miniGrid length in the future...)
 async function main() {
     await generateBtnClicked();
+
+    console.log(gridSizeValue);
 
     if (document.body.contains(miniGrid)) {
         gridFrame.replaceChildren();
@@ -41,4 +48,23 @@ async function main() {
     main();
 }
 
-main()
+
+
+async function hoverEngine() {
+    await main();
+
+    gridFrame.addEventListener("mouseover", eventDelegator);
+
+    function eventDelegator(e) {
+        if (e.target.matches("div")) {
+            e.target.style.cssText = "background-color: red;"
+        }
+    }
+
+    hoverEngine();
+}
+
+
+
+main();
+hoverEngine();
